6a6dec1de7562bdb68dd19603091f3b1d28e1921
TOYIN DIPO <toyindipol@gmail.com>
2015-05-22 16:30:07 +0100

    inclusion of ngstorage path in karma.conf.js file

--- karma.conf.js
+++ karma.conf.js
@@ -17,6 +17,7 @@ module.exports = function(config) {
     files: [
         'bower_components/angular/angular.js',
         'bower_components/angular-mocks/angular-mocks.js',
+        'bower_components/ngstorage/ngStorage.min.js',
         'js/thirdparty/*.js',
         'js/app/app.js',
         'js/directive/incexp.js',
